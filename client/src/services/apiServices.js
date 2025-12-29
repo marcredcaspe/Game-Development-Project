2dfbaa8c9d1286b78a1b42ec5248ec9b49da99ab
marcredcaspe <marcredcaspe@gmail.com>
2025-12-29 17:58:13 +0800

    Update apiServices.js

--- client/src/services/apiServices.js
+++ client/src/services/apiServices.js
@@ -1,14 +1,5 @@
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
 
-export const login = async (credentials) => {
-  const response = await fetch(`${API_URL}/login`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(credentials)
-  });
-  return response.json();
-};
-
 export const initApiService = () => console.log('API Online');
 
 export const login = async (username, password) => {
